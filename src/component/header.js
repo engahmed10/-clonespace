b2e04ee2ce4c38730bbff5592aeff14eae1d11d6
AHMED <eng.computer1@yahoo.com>
2021-04-22 06:53:06 -0500

    addeddheader

--- src/component/header.js
+++ src/component/header.js
@@ -1,6 +1,33 @@
-import react from 'react';
 import header from './header.css';
-const Heaser = (props) => {
-    return 
+import Slogo from './logo.js'
+import Hamburger from 'hamburger-react'
+import React, { useState } from 'react'
+
+
+const Header = (props) => {
+
+    const [isOpen, setOpen] = useState(false)
+
+    return <div className="header">
+             
+                      <div className="logo">
+                         <Slogo/>
+                      </div>
+
+                      <div className="center">
+                        <p>Falco9</p>
+                        <p>Falco Heavy</p>
+                        <p>Dragon</p>
+                        <p>Starship</p>
+                        <p>Human SpaceFlight</p>
+                        <p>RideShare</p>
+                      </div>
+
+                      <div className="right">
+                        <p>Shop</p>
+                         <Hamburger toggled={isOpen} toggle={setOpen} />
+                      </div>
+
+           </div>
 };
-export default header;
+export default Header;
