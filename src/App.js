4968cce2a15696c470f177befe111b7dc9f8e7ad
AHMED <eng.computer1@yahoo.com>
2021-04-22 23:41:27 -0500

    CREW-2 body text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
 import Header from './component/header'
 import Page from './component/page'
+import xspace from './images/xspace.jpg'
+import xspace2 from './images/xspace2.jpg'
+import xspace3 from './images/xspace3.jpg'
+import xspace4 from './images/xspace4.jpg'
+
 function App() {
   return (
     <div className="App">
          <Header/>
-         <Page/>
+         <Page
+            backgroundpic={xspace}
+            text1={" Upcoming Launch "}
+            text2={" Crew-2 Mission "}
+            btn={<button type="">Learn More</button>}
+         />
+          <Page
+             backgroundpic={xspace2}
+            text1={""}
+            text2={""}
+            button={"Learn More"}
+         />
+          <Page
+             backgroundpic={xspace3}
+            text={""}
+         
+         />
+          <Page
+             backgroundpic={xspace4}
+            text={""}
+         
+         />
+
     </div>
   );
 }
--- src/component/page.js
+++ src/component/page.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import header from './header.css';
 import xspace from '../images/xspace.jpg'
-const Page = () => {
+import page from './page.css'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(3),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+    
+  },
+  outlined:{
+
+  },
+}));
+
+
+const Page = ({backgroundpic,text1,text2,btn}) => {
+   const classes = useStyles();
     return (
         <div  claaName="container" >
        
-              <div  className="image" style={{width:'100vw',height:'100vh', backgroundImage:`url(${xspace})`,  
-              backgroundPosition: 'center',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat'
-
+              <div  className="image" style={{width:'100vw',height:'100vh', backgroundImage:`url(${backgroundpic})`,  
+                      backgroundPosition: 'center',
+                       backgroundSize: 'cover',
+                       backgroundRepeat: 'no-repeat'
                 }}>
-
+              <div className="body-container">
+                   <div className="text">
+                         <div className="text1">
+                             {text1}
+                         </div>
+                         <div className="text2">
+                             {text2}
+                         </div>
+                         <div className="button">
+                          {btn}
+                          
+                         </div>
+                         
+                    </div>
+              
+              </div>  
+              
                
                </div>
         </div>
